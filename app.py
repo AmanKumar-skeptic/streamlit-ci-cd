4a311cff285734b994d9bda7ca640b6424d819d5
Aman Kumar <105311518+AmanKumar-skeptic@users.noreply.github.com>
2024-07-30 19:53:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def fetch_weather():
 def main():
     st.title('Weather Report Live')
     st.write('Current Weather Conditions')
-    st.writre('Test03')
+    st.write('Test04')
 
     # Fetch weather data
     weather_data = fetch_weather()
